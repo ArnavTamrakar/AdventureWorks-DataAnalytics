231b3770e76e28eb6d541d60f321b7425ee8439e
arnav <144584534+ArnavTamrakar@users.noreply.github.com>
2024-08-20 23:27:33 +0930

    Add files via upload

---
+++ Customer SQL script.sql
@@ -0,0 +1,38 @@
+-- ALL DATA REQUIRED FOR CUSTOMER DASHBOARD
+SELECT 
+	*
+FROM 
+	[dbo].[FactInternetSales] as i
+JOIN DimCustomer c ON i.CustomerKey = c.CustomerKey
+JOIN DimGeography g ON c.GeographyKey = g.GeographyKey
+
+
+-- CALCULATING CUSTOMER RETURN RATE
+
+-- Step 1: Count total unique customers
+WITH TotalCustomers AS (
+    SELECT COUNT(DISTINCT fs.CustomerKey) AS TotalCustomerCount
+    FROM FactInternetSales fs
+)
+
+-- Step 2: Identify returning customers (those with more than one order)
+, ReturningCustomers AS (
+    SELECT CustomerKey
+    FROM FactInternetSales 
+    GROUP BY CustomerKey
+    HAVING COUNT(SalesOrderNumber) > 1
+)
+
+-- Step 3: Count total returning customers
+, TotalReturningCustomers AS (
+    SELECT COUNT(CustomerKey) AS ReturningCustomerCount
+    FROM ReturningCustomers
+)
+
+-- Step 4: Calculate returning rate
+SELECT 
+    (CAST(ReturningCustomerCount AS DECIMAL) / TotalCustomerCount) * 100 AS ReturningRate
+FROM TotalCustomers, TotalReturningCustomers;
+
+
+
---
+++ Order SQL script.sql
@@ -0,0 +1,77 @@
+-- ALL DATA RELATED TO ORDERS
+SELECT 
+	*
+FROM
+	FactInternetSales f
+JOIN DimGeography g ON f.SalesTerritoryKey = g.SalesTerritoryKey
+JOIN DimProduct p ON f.ProductKey = p.ProductKey
+
+
+-- HIGHEST NUMBER OF ORDERS BY COUNTRY
+SELECT 
+	COUNT(SalesOrderNumber) AS number_of_order,
+	g.SalesTerritoryCountry
+FROM 
+	[dbo].[FactInternetSales] i
+	JOIN DimSalesTerritory g
+	ON G.SalesTerritoryKey = i.SalesTerritoryKey
+GROUP BY g.SalesTerritoryCountry
+ORDER BY COUNT(SalesOrderNumber) DESC
+
+-- TOP 10 BEST SELLERS
+SELECT 
+	p.[EnglishProductName],
+	COUNT(*) AS OrderCount
+FROM 
+	FactInternetSales i
+	JOIN DimProduct p
+	ON i.ProductKey = p.ProductKey
+GROUP BY 
+	p.[EnglishProductName]
+ORDER BY 
+	COUNT(*) DESC
+
+-- NUMBER OF ORDER BY MONTH
+SELECT 
+    DATENAME(MONTH, OrderDate) AS MonthName,
+    COUNT(*) AS OrderAmount,
+	MONTH(OrderDate)
+FROM 
+    FactInternetSales
+GROUP BY 
+    DATENAME(MONTH, OrderDate), 
+    MONTH(OrderDate)
+ORDER BY 
+    MONTH(OrderDate)
+
+-- NUMBER OF ORDERS BY YEAR
+SELECT 
+	YEAR(d.FullDateAlternateKey) AS 'Year',
+	COUNT(*) AS OrderAmount
+FROM 
+	FactInternetSales i
+	JOIN DimDate d
+	ON d.DateKey = i.OrderDateKey
+GROUP BY 
+	YEAR(d.FullDateAlternateKey)
+ORDER BY
+	YEAR(d.FullDateAlternateKey)
+
+-- AVERAGE MOENY SPENT ON AN ORDER
+SELECT 
+	AVG(TotalProductCost) AS AvgMoneySpent 
+FROM 
+	[dbo].[FactInternetSales]
+
+-- AVERAGE ORDER PER DAY
+SELECT 
+    AVG(DailyOrderCount) AS AverageOrdersPerDay
+FROM (
+    SELECT 
+        CONVERT(DATE, OrderDate) AS OrderDate,  
+        COUNT(*) AS DailyOrderCount
+    FROM 
+        DimInternetSales  
+    GROUP BY 
+        CONVERT(DATE, OrderDate)  
+) AS DailyOrders;
